c3bd7193c507ff6ca0331f7500c4a77ba1305bb1
lix1898 <lix1898@126.com>
2021-08-29 11:58:58 +0800

    最简易的流程，省去了 beanFactory 以及循环依赖等流程，仅是个demo，有利于熟悉spring的正常流程

---
+++ src/com/xiaoyao/AppConfig.java
@@ -0,0 +1,8 @@
+package com.xiaoyao;
+
+import com.spring.ComponentScan;
+
+@ComponentScan("com.xiaoyao.service")
+public class AppConfig
+{
+}
---
+++ src/com/xiaoyao/MainTest.java
@@ -0,0 +1,19 @@
+package com.xiaoyao;
+
+import com.spring.AnnotationConfigApplicationContext;
+import com.xiaoyao.service.OrderService;
+
+public class MainTest
+{
+    public static void main(String[] args)
+    {
+        //启动Spring
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        //getBean()
+        OrderService orderService1=(OrderService)applicationContext.getBean("orderService");
+
+        System.out.println(orderService1);
+
+        orderService1.test();
+    }
+}
